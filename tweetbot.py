6280f9239456aae6debca876447ddf58adf652db
alipnick <46055014+alipnick@users.noreply.github.com>
2020-01-16 11:43:16 -0500

    Add files via upload
    Suggested re-write using the keyring package to avoid plaintext keys in secrets.txt

---
+++ tweetbot.py
@@ -0,0 +1,64 @@
+#/*
+# * ------------------------------------------------------------
+# * "THE BEERWARE LICENSE" (Revision 42):
+# * cfultz (Caleb Fultz) wrote this code. As long as you retain this 
+# * notice, you can do whatever you want with this stuff. If we
+# * meet someday, and you think this stuff is worth it, you can
+# * buy me a beer in return.
+# * ------------------------------------------------------------
+#*/
+# bot.py
+
+import tweepy
+import time
+import sys
+import keyring
+
+def set_auth(
+
+# This allows for you to pass a text file of your choosing to tweet each line
+argfile = str(sys.argv[1]) 
+if not argfile:
+    print('Expected input file as system argument, exiting')
+    return SystemExit
+
+# From Tweepy OAuth documentation
+#
+# Create an OAuthHandler instance
+# Twitter requires all requests to use OAuth for authentication
+try:
+    auth = tweepy.OAuthHandler(keying.get_password('consumer','key'),
+            keyring.get_password('consumer','secret'))
+    auth.set_access_token(keyring.get_password('access','token'),
+            keyring.get_password('access','secret'))
+# If you haven't run this before or have not set the valid info, you will be
+# prompted to enter the information
+except:
+    import getpass
+    keyring.set_password('consumer','key',
+            getpass.getpass(prompt='Please provide the consumer key: '))
+    keyring.set_password('consumer','secret',
+            getpass.getpass(prompt='Please provide the consumer password: '))
+    keyring.set_password('access','token',
+            getpass.getpass(prompt='Please provide the access token: '))
+    keyring.set_password('access','secret',
+            getpass.getpass(prompt='Please provide the access password: '))
+    auth = tweepy.OAuthHandler(keying.get_password('consumer','key'),
+            keyring.get_password('consumer','secret'))
+    auth.set_access_token(keyring.get_password('access','token'),
+            keyring.get_password('access','secret'))
+
+#Construct the API instance
+api = tweepy.API(auth) 
+
+# Parse the given file
+with open(argfile,'r') as f:
+    f=filename.readlines()
+
+# For every line in the file, process it as a status update.
+for line in f:
+    api.update_status(status=line)
+    time.sleep(3600) # Tweet every 1 hour for rate limiting.
+### Note that the time can be shortened significantly lower, however
+### you need to follow the Twitter guidelines
+### or you'll have your bot and account banned.
